1b641f93d9dad65b33ec81d29dc83f9309328498
Herbert Jones <herbert.jones@mediafire.com>
2015-09-02 12:45:26 -0500

    Re-generate templates

--- src/mediafire_sdk/api/user/link_gmail/v1_3.cpp
+++ src/mediafire_sdk/api/user/link_gmail/v1_3.cpp
@@ -71,7 +71,7 @@ void Impl::BuildUrl(
     *path = api_path + ".php";
 }
 
-void Impl::ParseResponse( Response * /* response */ )
+void Impl::ParseResponse( Response * response )
 {
     // This function uses return defines for readability and maintainability.
 #   define return_error(error_type, error_message)                             \
@@ -80,7 +80,8 @@ void Impl::ParseResponse( Response * /* response */ )
         return;                                                                \
     }
 
-
+    // Always have response_data on success.
+    response->response_data = ResponseData();
 
 #   undef return_error
 }
