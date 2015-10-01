890084c6fbf91af1d9e2cadbfcb2c8fe606ab4ed
Zachary Marquez <zachary.marquez@mediafire.com>
2015-10-01 14:38:06 -0500

    BUG VS 2015 supports noexcept

--- src/mediafire_sdk/utils/noexcept.hpp
+++ src/mediafire_sdk/utils/noexcept.hpp
@@ -7,8 +7,8 @@
  */
 #pragma once
 
-// Visual Studio gets no noexcept.
-#ifndef _MSC_VER
+// Visual Studio gets no noexcept until VS 2015
+#if !defined(_MSC_VER) || _MSC_VER >= 1900
 /** noexcept if supported on plaform. */
 #define NOEXCEPT noexcept
 #else
